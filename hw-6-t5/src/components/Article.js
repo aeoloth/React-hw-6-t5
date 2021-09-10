3d1ffe7234d01468761b9935b0f9d5d1ee8cff51
Eldar Zainashev <aeoloth@yandex.ru>
2021-09-11 00:33:29 +0300

    h

--- hw-6-t5/src/components/Article.js
+++ hw-6-t5/src/components/Article.js
@@ -1,73 +1,66 @@
-import React, {Component} from 'react'
-import '../App.css'
-
+import React, { Component } from "react";
+import "../App.css";
 
 class Article extends Component {
-    constructor(props) {
-        super(props)
+  constructor(props) {
+    super(props);
 
-        this.state = {
-            isOpen: props.defaultOpen
-        }
+    this.state = {
+      isOpen: props.defaultOpen,
+    };
+  }
+
+  componentWillMount() {
+    console.log("---", "mounting");
+  }
+
+  componentWillReceiveProps(nextProps) {
+    console.log("---", "will recieve props");
+    if (nextProps.defaultOpen !== this.props.defaultOpen) {
+      this.setState({
+        isOpen: nextProps.defaultOpen,
+      });
     }
+  }
 
+  componentWillUpdate() {
+    console.log("---", "will update");
+  }
 
-    shouldComponentUpdate(nextProps, nextState) {
-        return this.state.isOpen !== nextState.isOpen
-    }
+  render() {
+    const { article } = this.props;
+    const style = { width: "50%" };
+    const body = this.state.isOpen && (
+      <section className="card-text">{article.text}</section>
+    );
+    return (
+      <div className="card mx-auto" style={style}>
+        <div className="card-header">
+          <h2>
+            {article.title}
+            <button
+              button
+              onClick={this.handleClick}
+              className="btn btn-primary btn-lg float-right"
+            >
+              {this.state.isOpen ? "close" : "open"}
+            </button>
+          </h2>
+        </div>
+        <div className="card-body">
+          <h6 className="card-subtitle text-muted">
+            creation date: {new Date(article.date).toDateString()}
+          </h6>
+          {body}
+        </div>
+      </div>
+    );
+  }
 
-
-    componentWillMount() {
-        console.log('---', 'mounting')
-    }
-
-
-    componentWillReceiveProps(nextProps) {
-        console.log('---', 'will recieve props')
-        if (nextProps.defaultOpen !== this.props.defaultOpen) {this.setState({
-            isOpen: nextProps.defaultOpen
-        })}
-    }
-
-
-    componentWillUpdate() {
-        console.log('---', 'will update')
-    }
-
-    render() {
-        const {article} = this.props
-        const style = {width: '50%'}
-        const body = this.state.isOpen && <section className="card-text">{article.text}</section>
-        return (
-            <div className="card mx-auto" style = {style}>
-                <div className="card-header">
-                    <h2>
-                        {article.title}
-                        <button button onClick = {
-                            this.handleClick
-                        }
-                        className = "btn btn-primary btn-lg float-right" >
-                            {this.state.isOpen ? 'close' : 'open'}
-                        </button>
-                    </h2>
-                </div>
-                <div className="card-body">
-                    <h6 className="card-subtitle text-muted">
-                        creation date: {(new Date(article.date)).toDateString()}
-                    </h6>
-                    {body}
-                </div>
-            </div>
-        )
-    }
-
-    handleClick = () => {
-        console.log('---', 'clicked');
-        this.setState(
-            {isOpen: !this.state.isOpen}
-        )
-    }
+  handleClick = () => {
+    console.log("---", "clicked");
+    this.setState({ isOpen: !this.state.isOpen });
+  };
 }
 
-
-export default Article
+export default Article;
